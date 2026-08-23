e918373ca276b31b23bd9ac609944662d47cbcbd
dilpenny <diegoname79@gmail.com>
2026-08-23 09:25:18 +0100

    ...

--- android/app/src/main/java/com/social/opringo/MainActivity.java
+++ android/app/src/main/java/com/social/opringo/MainActivity.java
@@ -1,19 +1,5 @@
 package com.social.opringo;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        WebView webView = getBridge().getWebView();
-
-        webView.setWebViewClient(new WebViewClient());
-    }
-}
+public class MainActivity extends BridgeActivity {}
